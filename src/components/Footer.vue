95374ff6a77de957783860e2b338e97e5e59996d
NCC-17039 <120711536+Bashir-1@users.noreply.github.com>
2023-04-14 16:21:15 +0800

    更新页脚

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -44,7 +44,7 @@ footer {
     margin-bottom: 4px;
     .description {
       &::after {
-        content: "@ Copyright By";
+        content: "@ Copyright By NCC-17039";
         margin: 0 6px;
       }
     }
